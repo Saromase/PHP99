4e8349075aa94745c12b9f6006afc07f77cfac5b
Saromase <seite.romain@hotmail.fr>
2017-06-22 01:59:30 +0200

    Add Count lenght string

---
+++ fonction/count_string.php
@@ -0,0 +1,15 @@
+<pre>
+<?php
+$phrase = ["ah...", "encore un beau dimanche", "de perdu", "cloîtré chez", "vous", "à coder", ",mais", " vous avez signé pour ça pas vrai ?", "alors courage !"];
+
+for ($i = 0;$i < count($phrase) -1; $i++){
+    $motA = $phrase[$i];
+    $motB = $phrase[$i+1];
+    if (strlen($motA) > strlen($motB)){
+        $phrase[$i] = $motB;
+        $phrase[$i +1] = $motA;
+    }
+    }
+print_r($phrase[(count($phrase)-1)]);
+?>
+</pre>
--- index.php
+++ index.php
@@ -9,5 +9,6 @@
       <a href="fonction/string_to_array.php">Exercice 2</a>
       <a href="fonction/select_last.php">Exercice 3</a>  
       <a href="fonction/delete_middle.php">Exercice 4</a>
+      <a href="fonction/count_string.php">Exercice 5</a>
     </html>
 </body>
