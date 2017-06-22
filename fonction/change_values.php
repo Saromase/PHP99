867d98a6bf22ffae3acb92811d21e9d617f4aba8
Romain <seite.romain@yahoo.com>
2017-06-22 14:24:21 +0200

    refactoring change_values

--- fonction/change_values.php
+++ fonction/change_values.php
@@ -1,16 +1,15 @@
 <?php include '../include/header.php';
 echo '<div class="text-center"><h1>Exercice 6</h1></div>';
 $portables = ["0612459623", "0645896235", "0678145263", "0647895263", "0655069912"];
-$a = '06';
-$b = '+33';
+$input_user = '06';
+$new_input = '+33';
 for ($i = 0; $i < count($portables); $i++){
-    $c = $portables[$i];
-    $d = substr($c,0,2);
-    $e = substr($c,2,10);
-    $f = str_replace($c, $b, $c);
-    $newPortables[$i] = $f.$e;
+    $numero = $portables[$i];
+    $first_num = substr($numero,2,10);
+    $replace = str_replace($numero, $new_input, $numero);
+    $newPortables[$i] = $replace.$first_num;
 }
-echo '<div class="text-center">La transformation du début des numéros de téléphone commencant par ' . $a . ' en ' . $b . ' a bien était effectué</div>';
+echo '<div class="text-center">La transformation du début des numéros de téléphone commencant par ' . $input_user . ' en ' . $new_input . ' a bien était effectué</div>';
 ?>
 <div class="table-responsive">          
   <table class="table">
